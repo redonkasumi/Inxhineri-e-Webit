2dc94dcdf0067cd29a211337bb9bf97fa3318fe4
yllkaselimi <96298157+yllkaselimi@users.noreply.github.com>
2022-02-27 16:52:37 +0100

    Add files via upload

--- js/signUp.js
+++ js/signUp.js
@@ -3,36 +3,20 @@ function checkMatching(form) {
     var password2 = form.password2.value;
 
 
-    // If password not entered 
     if (password == '')
         alert("Please enter Password");
 
-    // If confirm password not entered 
     else if (password2 == '')
         alert("Please enter confirm password");
 
-    // If Not same return False.     
     else if (password != password2) {
         alert("\nPassword did not match: Please try again...");
         return false;
     }
 
-    // If same return True. 
     else {
 
-        //check for terms and conditions
-        var check = document.getElementById("terms").checked;
-        if (!check) {
-            alert('You must agree to the terms first.');
-            return false;
-        }
-        //after check return true
-        alert("Welcome!");
-        var user = form.username.value;
-        localStorage.setItem("user_name", user);
-        var pass = form.password1.value;
-        localStorage.setItem("pass_word", pass);
-        window.location.href = 'index.html';
+        window.location.href = 'index.php';
 
         return true;
     }
